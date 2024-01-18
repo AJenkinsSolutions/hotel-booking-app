d0dbaf4819f3a7c5857e5cf7073cd8a5b5b03ebf
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-01-18 16:24:32 -0600

    Add: FormBuilder, form validators

--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,11 +7,43 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css']
 })
-export class BookingFormComponent {
+export class BookingFormComponent implements OnInit{
 
   bookingForm: FormGroup = new FormGroup({});
+  
+  constructor(private fb: FormBuilder){}
 
-  onSubmit(){};
+  ngOnInit(): void {
+    
+    this.bookingForm = this.fb.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]]
+
+    })
+    
+  }
+
+  
+
+  
+
+  
+
+
+  onSubmit(){
+    console.log("Entered onSubmit()");
+
+    if(this.bookingForm.valid){
+
+      console.log("Form is Valid")
+    }else{
+      console.log("Form is invalid")
+    }
+
+
+  };
 
 
 }
