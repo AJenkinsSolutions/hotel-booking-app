587ddbdca9255863809b467ed3b0d64b31e9fe42
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-01-25 17:27:16 -0600

    Add:  Booking service

--- src/app/booking/booking.service.ts
+++ src/app/booking/booking.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Booking } from '../models/booking';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,33 @@ import { Injectable } from '@angular/core';
 export class BookingService {
 
   constructor() { }
+
+
+  private bookings: Booking[] = [];
+3
+
+  //CRUD Operations
+  getAllBookings(): Booking[]{
+    
+    
+    
+    return this.bookings;
+  }
+
+
+  getBooking(id: number): Booking | undefined{ 
+    return this.bookings.find(bok => bok.id == id);
+  
+  }
+
+  addbooking(booking: Booking): void{
+    this.bookings.push(booking);
+  }
+
+  deleteBooking(id: number): string | void{
+
+    let index = this.bookings.findIndex(bok => bok.id === id)
+    this.bookings.splice(index, 1)
+  }
+
 }
