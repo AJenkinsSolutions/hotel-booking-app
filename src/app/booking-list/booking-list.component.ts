728bf5188d115737d1c3be3d8c2f7eb6740c3754
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-01-30 14:37:41 -0600

    Add: Booking List html, component, impl

--- src/app/booking-list/booking-list.component.ts
+++ src/app/booking-list/booking-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgModelGroup } from '@angular/forms';
+import { BookingService } from '../booking/booking.service';
+import { Booking } from '../models/booking';
 
 @Component({
   selector: 'app-booking-list',
   templateUrl: './booking-list.component.html',
   styleUrls: ['./booking-list.component.css']
 })
-export class BookingListComponent {
+
+export class BookingListComponent implements OnInit{
+  
+  bookingArray: Booking[] = []
+  
+  constructor(private bookingService: BookingService){
+  }
+  
+  
+  ngOnInit(): void {
+
+    const savedBookings = this.bookingService.getAllBookings();
+
+   if( savedBookings!= null){
+
+    this.bookingArray = savedBookings;
+
+   }else{
+    console.log("No bookings saved")
+   }
+
+
+  }
 
 }
